9b50b6c2ec27dbff1437b7865273659723ae7428
Robin <156453367+hbgroca@users.noreply.github.com>
2025-05-11 12:30:44 +0200

    Base Configuration

---
+++ WebApi/Controllers/AccountsController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApi.Services;
+
+namespace WebApi.Controllers;
+[Route("api/[controller]")]
+[ApiController]
+public class AccountsController(IAccountService accountService) : ControllerBase
+{
+    private readonly IAccountService _accountService = accountService;
+}
--- WebApi/Program.cs
+++ WebApi/Program.cs
@@ -1,13 +1,39 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using WebApi.Data;
+using WebApi.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddOpenApi();
 
+builder.Services.AddScoped<IAccountService, AccountService>();
+builder.Services.AddDbContext<DataContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("AccountDbConnection")));
+
+builder.Services.AddIdentity<IdentityUser, IdentityRole>(options =>
+{
+    options.User.RequireUniqueEmail = true;
+    options.Password.RequiredLength = 6;
+    //options.SignIn.RequireConfirmedAccount = false;
+
+    //options.Password.RequireDigit = false;
+    //options.Password.RequireNonAlphanumeric = false;
+    //options.Password.RequireUppercase = false;
+    //options.Password.RequireLowercase = false;
+}).AddEntityFrameworkStores<DataContext>()
+    .AddDefaultTokenProviders();
+
 var app = builder.Build();
 
 app.MapOpenApi();
-
+app.UseCors(x => x
+    .AllowAnyOrigin()
+    .AllowAnyMethod()
+    .AllowAnyHeader());
 app.UseHttpsRedirection();
+app.UseAuthentication();
 app.UseAuthorization();
 app.MapControllers();
 app.Run();
